06ff88df0cfb510c7a7f4c6f7e869af601de7dc6
Matteo Bucciol <62759388+matteobu@users.noreply.github.com>
2024-02-02 10:51:59 +0100

    change copy with new

--- src/components/PluginPresets/index.tsx
+++ src/components/PluginPresets/index.tsx
@@ -77,7 +77,7 @@ const PluginPresets: React.FC<IPresetsProps> = ({
       presetName || DEFAULT_PLUGIN_SETTINGS.presets[0].name + ' ' + _pluginPresets.length;
     const nameExists = isPresetNameAlreadyExists(_presetName, _pluginPresets, currentPresetIdx);
     if (nameExists && type === 'new') {
-      _presetName += ' copy';
+      _presetName += ' New';
       setPresetNameAlreadyExists(false);
     } else if (nameExists) {
       setPresetNameAlreadyExists(true);
@@ -164,8 +164,8 @@ const PluginPresets: React.FC<IPresetsProps> = ({
   };
 
   const handleDragEnter = (e: React.DragEvent<HTMLDivElement>, index: number) => {
-    e.stopPropagation()
-    console.log("hi")
+    e.stopPropagation();
+    console.log('hi');
     setDragOverItemIndex(index);
   };
 
