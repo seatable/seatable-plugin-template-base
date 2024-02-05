896638874116ae64cb265b1c0bb03d0adf85b7cc
Matteo Bucciol <62759388+matteobu@users.noreply.github.com>
2024-02-05 10:14:05 +0100

    (bug): #49 Trim table and view name in settings

---
+++ src/utils/helpers.ts
@@ -0,0 +1,11 @@
+export const truncateTableName = (tableName: string) => {
+  let _tableName;
+
+  if (tableName.split('').length > 22) {
+    _tableName = tableName.slice(0, 20) + '...';
+
+    return _tableName;
+  }
+
+  return tableName;
+};
