62d82ad78382a6db65165c36046018557721fc7b
Matteo Bucciol <62759388+matteobu@users.noreply.github.com>
2024-02-23 14:00:55 +0100

    fix: #153 Remove hover

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -68,16 +68,6 @@ const Header: React.FC<IHeaderProps> = (props) => {
         <span className={styles.modal_header_icon_btn} onClick={printPdfDocument}>
           <span className="dtable-font dtable-icon-print"></span>
         </span>
-        <span
-          className={`${styles.modal_header_icon_btn} ${
-            isShowSettings ? styles.modal_header_icon_btn_active : ''
-          }`}
-          onClick={toggleSettings}>
-          <span className="dtable-font dtable-icon-set-up"></span>
-          {isShowSettings && (
-            <span className={styles.modal_header_icon_btn_settings}>Settings</span>
-          )}
-        </span>
         <span className={styles.modal_header_icon_btn} onClick={togglePlugin}>
           <span className="dtable-font dtable-icon-x btn-close"></span>
         </span>
