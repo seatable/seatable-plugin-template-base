14af6622830180d8ba40699062715e1dff0ba375
Abisola Adebiyi <abisola.ca@gmail.com>
2024-02-26 18:49:03 -0800

    (header): #131 Add functionality to resize modal

---
+++ src/components/ResizableWrapper/index.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useRef } from 'react';
+import styles from '../../styles/Modal.module.scss';
+import { IResizableWrapper } from '../../utils/Interfaces/ResizableWrapper.interface';
+
+const ResizableWrapper:React.FC<IResizableWrapper> = ({ children }) => {
+  const modalRef = useRef<null | HTMLDivElement>(null);
+  const modalTop = useRef<null | HTMLDivElement>(null);
+  useEffect(() => {
+    let resizableModal = modalRef.current;
+    let style;
+    let height: number;
+    if (resizableModal) {
+      style = window.getComputedStyle(resizableModal);
+      height = parseInt(style.height, 10);
+    }
+    let yCord = 0;
+    let topResizer = modalTop.current;
+
+    const onMouseMoveTopResize = (event: MouseEvent) => {
+      const dy = event.clientY - yCord;
+      height = height - dy;
+      yCord = event.clientY;
+      if (resizableModal) {
+        resizableModal.style.height = `${height}px`;
+      }
+    };
+    const onMouseUpTopResize = (event: MouseEvent) => {
+      document.removeEventListener('mousemove', onMouseMoveTopResize);
+    };
+    const onMouseDownTopResize = (event: MouseEvent) => {
+      yCord = event.clientY;
+      if (resizableModal) {
+        style = window.getComputedStyle(resizableModal);
+        resizableModal.style.bottom = style.bottom;
+        resizableModal.style.top = '';
+      }
+      document.addEventListener('mousemove', onMouseMoveTopResize);
+      document.addEventListener('mouseup', onMouseUpTopResize);
+    };
+    topResizer?.addEventListener('mousedown', onMouseDownTopResize);
+
+    return () => {
+      topResizer?.removeEventListener('mousedown', onMouseDownTopResize);
+    };
+  }, [modalRef]);
+
+  return (
+    <div ref={modalRef} className={styles.modal_wrapper}>
+      {/* draggable  */}
+      <div ref={modalTop} className={styles.modal_draggable}></div>
+      {children}
+    </div>
+  );
+};
+
+export default ResizableWrapper;
