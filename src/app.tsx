0b327e6aceabdc23f37beddff50a22ddefa12d7a
Matteo Bucciol <62759388+matteobu@users.noreply.github.com>
2024-02-28 10:20:29 +0100

    Fix crash when no values

--- src/app.tsx
+++ src/app.tsx
@@ -59,8 +59,6 @@ const App: React.FC<IAppProps> = (props) => {
   const { activeTable, activePresetId, activePresetIdx, activeViewRows } = appActiveState;
 
   useEffect(() => {
-    // window.dtableSDK.updatePluginSettings(PLUGIN_NAME, DEFAULT_PLUGIN_DATA);
-
     initPluginDTableData();
     return () => {
       unsubscribeLocalDtableChanged();
