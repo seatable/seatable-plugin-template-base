e93aba9ef15658601cb13a4e48f84785ca90c3bb
Christoph Dyllick-Brenzinger <cdb@datamate.org>
2024-03-07 13:51:29 +0100

    change of README.md

--- src/setting.local.dist.js
+++ src/setting.local.dist.js
@@ -2,14 +2,14 @@
 local development settings
 */
 export default {
-  // base api token (required)
-  APIToken: '01ab23[...]45cd67',
   // server URL of the seatable server of the plugin (required)
   server: 'https://cloud.seatable.io',
-  // id of the workspace with the dtable of the plugin (required, workspace must exist)
-  workspaceID: '1',
   // name of the dtable to add the plugin to (required, dtable must exist under this name)
   dtableName: 'Default',
+  // base api token (required)
+  APIToken: '01ab23[...]45cd67',
+  // id of the workspace with the dtable of the plugin (required, workspace must exist)
+  workspaceID: '1',
   // default language ('en' or 'zh-cn' are common, see "src/locale/index.js" for all lang keys)
   lang: 'en',
   // show verbose messages
